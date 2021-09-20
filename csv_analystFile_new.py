c03ef2fdd36b98fa82c412bbccab0fbc6cd9be4b
nat-andr <90087021+nat-andr@users.noreply.github.com>
2021-09-20 21:02:41 +0300

    vosst myListBox

--- csv_analystFile_new.py
+++ csv_analystFile_new.py
@@ -7,74 +7,89 @@ from tkinter import Menu #$$$$$$$$$$$$$$$
 import os
 import pandas as pd
 
+df=[]
+my_lst=[]
+lst2=[]
+idx=0
+cnt_rows=0
+cnt_columns=0
+file_name=""
+
 # Create main window
 myWin=tk.Tk()
 myWin.geometry("650x650")
 myWin.title("Analyst of .csv files")
-
-# Create listbox for choosing field  @@@@@@@@@@@@@@@@@@@
 myListBox=tk.Listbox(myWin,selectmode='browse')
 myListBox.grid(row=5,column=0)
 
-##############################   functions    #################################
-# openning file dialog
-def do_dialog():
-    my_dir=os.getcwd()
-    name=fd.askopenfilename(initialdir=my_dir)
-    return name
 
-# reading csv
-def pandas_read_csv(file_name):
-        df=pd.read_csv(file_name,header=None,sep=';')
-        cnt_rows=df.shape[0]
-        cnt_columns=df.shape[1]
-        label_11['text']=cnt_rows
-        label_21['text']=cnt_columns
-        return df
+
+##############################   functions    #################################
 
 
 # menu file
-def open_csv_file():
-    file_name=do_dialog()
+def open_and_read_csv():
+    my_dir=os.getcwd()
+    file_name=fd.askopenfilename(initialdir=my_dir)
     label_01['text']=file_name
-    df=pandas_read_csv(file_name)
- #   
-    lst2=df.shape[1]
-    for j in range(df.shape[0]):
-        myListBox.insert(tk.END,lst2[j])
-    return lst2
+    df=pd.read_csv(file_name,header=None,sep=';')
+    
+    cnt_rows=df.shape[0]
+    cnt_columns=df.shape[1]
+    label_11['text']=cnt_rows
+    label_21['text']=cnt_columns
+    
+    row = df.iloc[0] 
+    print(row[0])
 
+    #print(cols)
+    
+    for j in range(cnt_columns-1):
+        print(row[j])
+        myListBox.insert(tk.END,row[j])   
 
+    onselect(myListBox.selection_set)
+    myListBox.bind('<<ListboxSelect>>', clickEvent)     
+    sel_col=idx
+    lst=get_column(df,sel_col)
+    for item in get_column(df,5):
+        output_text.insert(tk.END, str(item)+os.linesep)
+
+    return df,lst, file_name, cnt_rows, cnt_columns
+
+#getting content of column
+def get_column(df,num_col):
+    cnt_rows=df.shape[0]
+#    kol_rows=df.shape[0]
+#    print(kol_rows)
+    my_lst=[]
+    for i in range(cnt_rows):
+        my_lst.append(df.iat[i,num_col])
+    return my_lst
+'''
+# handle click on Listbox
+
+def clickEvent:
+  #insert code that will execute here
+    w = event.widget
+    idx = int(w.curselection()[0])
+    value = w.get(idx)
+    print(value,idx) 
+#lb = Listbox(main)
+
+    myListBox.bind('<<ListboxSelect>>', clickEvent)
+    return idx
+'''
 # handle event
 def onselect(event):
     w = event.widget
     idx = int(w.curselection()[0])
     value = w.get(idx)
-    print(value)
+    print(value,idx)
+    
+    return idx
 
 
-myListBox.bind('<<ListboxSelect>>', onselect)
-
-
-# openning file dialog
-def do_dialog():
-    my_dir=os.getcwd()
-    name=fd.askopenfilename(initialdir=my_dir)
-    return name
-
-#getting content of column
-def get_column(df,column_ix):
-    cnt_rows=df.shape[0]
-    lst=[]
-    for i in range(cnt_rows):
-        lst.append(df.iat[i,column_ix])
-    return lst
-
-# getting num of columns
-def colSelection():
-    selection=MyListBox.curselection()
-    print(selection)
-    num_selection=MyListBox[0]
 
 ################################## finish of functions ###################################
 
@@ -87,15 +102,19 @@ label_00.grid(row=1, column=0,padx=10, pady=10, sticky="e")
 
 label_01=tk.Label(text="")
 label_01.grid(row=1, column=1, sticky="w")
+
 #-------------------
 label_10=tk.Label(text=" Rows: ")
 label_10.grid(row=2, column=0, padx=10, pady=10, sticky="e")
 
+
 label_11=tk.Label(text="")
 label_11.grid(row=2, column=1, sticky="w")
+
 #-------------------
 label_20=tk.Label(text=" Columns: ")
 label_20.grid(row=3, column=0, padx=10, pady=10, sticky="e")
+
 
 label_21=tk.Label(text="")
 label_21.grid(row=3, column=1, sticky="w")
@@ -103,10 +122,22 @@ label_21.grid(row=3, column=1, sticky="w")
 # Create text field for general output
 output_text=st(height=28,width=60)
 output_text.grid(row=5, column=1,padx=10, pady=10, sticky="w")
-'''
+
+
 # Create listbox for choosing field  @@@@@@@@@@@@@@@@@@@
-myListBox=tk.Listbox(myWin,selectmode="single")
-myListBox.grid(row=5,column=0)
+
+#get_headers(df)
+
+#myListBox.bind('<<ListboxSelect>>', onselect)
+'''
+# button-click_message
+def process_button():
+    file_name=do_dialog()
+    label_01['text']=file_name
+    df=pandas_read_csv(file_name)
+    lst=get_column(df,2)
+    for item in lst:
+        output_text.insert(tk.END, str(item)+os.linesep)
 '''
 
 
@@ -115,8 +146,8 @@ menubar = Menu(myWin)
 myWin.config(menu=menubar)
 
 filemenu=Menu(menubar,tearoff=0)
+filemenu.add_command(label="Open and read .csv file", command=open_and_read_csv)
 
-filemenu.add_command(label="Find file", command=open_csv_file)
 #filemenu.add_command(label="Read file", command=do_something)
 filemenu.add_command(label='Exit', command=myWin.destroy)
 filemenu.add_separator()
