5d77bdfd16f2221b9dca4be1a18e8ac3861831a2
nat-andr <90087021+nat-andr@users.noreply.github.com>
2021-09-24 23:45:32 +0300

    lines

--- csv_analystFile_new.py
+++ csv_analystFile_new.py
@@ -1,9 +1,12 @@
 # Analyst csv files
+
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText as St
 from tkinter import messagebox as mb
 from tkinter import filedialog as fd
 from tkinter import Menu
+from tkinter import Canvas, BOTH
+from tkinter import Frame
 import os
 from typing import Union, Any, Pattern
 
@@ -12,12 +15,20 @@ from pandas import Series, DataFrame
 from pandas.core.generic import NDFrame
 from pandas.io.parsers import TextFileReader
 from tkinter import messagebox as mb
-
+from tkinter import ttk
 import re
 
 
+
+#желтый
+foregroundColor = "#FFFACD"
+#коричневыq
+backgroundColor = "#800000"
+font="Times New Roman, 12"
+
 df = ()
 content_col = []
+content=[]
 lst2 = []
 idx = 0
 cnt_rows = 0
@@ -28,11 +39,45 @@ headers = []
 counter_OK = 0
 reliability_data=0
 
+
+
 # Create main window
 myWin = tk.Tk()
+#myWin.configure(background="#555")
 myWin.resizable(False,True)
 myWin.geometry("600x535")
 myWin.title("Analyse of .csv files")
+myWin["bg"]=foregroundColor
+
+
+
+
+
+
+# Create names of fields for output
+label_00=tk.Label(text=" Name of file:",bg=foregroundColor)
+label_00.grid(row=1, column=0,padx=10, pady=10, sticky="e")
+
+label_01=tk.Label(text="",bg=foregroundColor)
+label_01.grid(row=1, column=1, sticky="w")
+#-------------------
+label_10=tk.Label(text=" Rows: ",bg=foregroundColor)
+label_10.grid(row=2, column=0, padx=10, pady=10, sticky="e")
+
+label_11=tk.Label(text="",bg=foregroundColor)
+label_11.grid(row=2, column=1, sticky="w")
+#-------------------
+label_20=tk.Label(text=" Columns: ",bg=foregroundColor)
+label_20.grid(row=3, column=0, padx=10, pady=10, sticky="e")
+
+label_21=tk.Label(text="",bg=foregroundColor)
+label_21.grid(row=3, column=1, sticky="w")
+#myWin["titlebg"]="#800000"
+
+
+myCanvas = Canvas(myWin)
+
+
 
 myListBox = tk.Listbox(myWin, selectmode='browse')
 #myListBox.grid(row=5, column=0)
@@ -42,32 +87,13 @@ output_text = St(height=28, width=60)
 #output_text.place(row=5, column=1, padx=10, pady=10, sticky="w")
 output_text.place(x=250, y=125, width=310, height=375)
 
-# Create names of fields for output
-label_00 = tk.Label(text=" Имя файла:")
-label_00.place(x=83, y=60, width=26, height=13)
-
-label_01 = tk.Label(text="")
-label_01.place(x=156, y=60, width=390, height=13)
-
-# -------------------
-label_10 = tk.Label(text=" Строки: ")
-label_10.place(x=83, y=96, width=26, height=13)
-
-label_11 = tk.Label(text="")
-label_11.place(x=156, y=96, width=60, height=13)
-
-label_20 = tk.Label(text=" Столбцы: ")
-label_20.place(x=83, y=130, width=26, height=13)
-
-label_21 = tk.Label(text="")
-label_21.place(x=156, y=130, width=60, height=13)
 
 
-label_Click = tk.Label(text="Кликните по строке")
-label_Click.place(x=100, y=340, width=50, height=20)
 
-ent_search = tk.Entry(width=50)
-ent_search.place(x=300, y=600)
+
+entry_shablon = tk.Entry(width=25)
+entry_shablon.place(x=35, y=562)
+entry_shablon.insert(0,"")
 
 
 my_menu = Menu(myWin)
@@ -99,28 +125,31 @@ def open_and_read_csv():
         myListBox.insert(tk.END,row[j])
 
     myListBox.bind('<<ListboxSelect>>', callback)
-
+    
+    label_Click = tk.Label(text="Кликните по строке",bg=foregroundColor, fg="red")
+    label_Click.place(x=50, y=170, width=160, height=20)
+   
     return df, cnt_rows, cnt_columns, file_name
 
 def resize():
     #myWin.geometry("")
-    myWin.geometry("600x750")
+    myWin.geometry("600x700")
 
 # menu file
-filemenu = Menu(my_menu, tearoff=0)
-filemenu.add_command(label="Open and read .csv file", command=open_and_read_csv)
+filemenu = Menu(my_menu,  tearoff=0)
+filemenu.add_command(label="Open and read .csv file", command=open_and_read_csv, background="#800000",foreground="#FFFACD")
 
 filemenu.add_separator()
-filemenu.add_command(label='Exit', command=myWin.destroy)
+filemenu.add_command(label='Exit', command=myWin.destroy, background="#800000",foreground="#FFFACD")
 my_menu.add_cascade(label="File", menu=filemenu)
 
 
 searchmenu = Menu(my_menu, tearoff=0)
-searchmenu.add_command(label="Searching", command=resize)
-#searchmenu.add_command(label="Picking")
-#searchmenu.add_separator()
+searchmenu.add_command(label="Searching", command=resize, background="#800000",foreground="#FFFACD")
+
 my_menu.add_cascade(label="Searching", menu=searchmenu)
-#getting content of column
+
+
 
 def get_column(n_col):
     global df
@@ -132,14 +161,14 @@ def get_column(n_col):
 #    print(kol_rows)
     for i in range(kol_rows):
         content_col.append(df.iat[i,sel_col])
-    return content_col
-
+        content=content_col
+    return content
  # handle event
 def callback(event):
     global sel_col
     global df
     global cnt_rows
-    global content_col
+    global content
     selection = event.widget.curselection()
     if selection:
         index = selection[0]
@@ -155,45 +184,92 @@ def callback(event):
         output_text.insert(tk.END, str(item) + os.linesep)
     return sel_col
 
+countOK_label=tk.Label(text="",bg=foregroundColor)
+countOK_label.place(x=100,y=660,width=50,height=13)
 
+countOK_name_label=tk.Label(text="Найдено записей",bg=foregroundColor)
+countOK_name_label.place(x=50,y=640,width=150,height=13)
+
+dostover_label=tk.Label(text="",bg=foregroundColor)
+dostover_label.place(x=350,y=660,width=40,height=13)
+
+dostover_name_label = tk.Label(text="Доля записей, соответствующих шаблону",bg=foregroundColor)
+dostover_name_label.place(x=250, y=640, width=300, height=13)
 
 def check():
     global sel_col
     global content_col
     global counter_OK
     global cnt_rows
+    global valChoice
+    global content
+
+    print("cnt_rows from start=",cnt_rows)
     my_shab: str=""
     counter_OK = 0
     reliability_data=0
-    my_shab = ent_search.get()
+    n_records=0
+    my_shab = entry_shablon.get()
 
     if len(my_shab) == 0:
-         mb.showinfo(title=None, message="This field cannot be empty! ")
+         mb.showinfo(title=None, message="")
     else:
         print("shablon=", my_shab)
         #regex=re.compile(my_shab)
-        for item in content_col:
-            #if (regex.match(my_shab)):
+        if valChoice==1:
+            n_records = cnt_rows
+            content=content_col
+            for i in range(cnt_rows):
+                content.append(df.iat[i, 1])
+        else:
+            n_records = cnt_rows * cnt_columns
+            for i in range(cnt_columns-1):
+                for j in range(cnt_rows):
+                    content.append(df.iat[i,j])
 
+        print("after vybor n_records=", n_records)
+        for item in content:
             if str.__contains__(str(item), my_shab):
-                counter_OK+=1
-        countOK_label['text']= str(counter_OK)
-        reliability_data= round(counter_OK/cnt_rows*100, 2)
-        dostover_label['text']=str(reliability_data)+"%"
-        print("counter_OK=",counter_OK, "  cnt_rows=",cnt_rows, "reliability_data=",reliability_data)
+                counter_OK += 1
+
+    countOK_label['text']= str(counter_OK)
+    reliability_data= round((counter_OK/n_records)*100, 2)
+    dostover_label['text']=str(reliability_data)+"%"
+    print("  counter_OK=",counter_OK, "  cnt_rows=",cnt_rows, "reliability_data=",reliability_data)
     return counter_OK
 
-but_check = tk.Button(myWin, text="Check", command=check)
-but_check.place(x=150,y=650,width=50,height=30)
 
-countOK_label=tk.Label(text="")
-countOK_label.place(x=200,y=650,width=50,height=30)
+but_check = tk.Button(myWin, text="Найти", command=check,background="#800000",foreground="#FFFACD", relief="raised")
+but_check.place(x=420,y=562,width=108,height=30)
 
-dostover_label=tk.Label(text="")
-dostover_label.place(x=350,y=650,width=250,height=30)
 
-dostover_name_label = tk.Label(text="Reliability of data")
-dostover_name_label.place(x=250, y=650, width=200, height=30)
+
+
+
+
+
+label_line1=tk.Label(text=".",bg='black')
+label_line1.place(x=0,y=537,width=600,height=3)
+
+label_line2=tk.Label(text=".",bg='black')
+label_line2.place(x=0,y=608,width=600,height=3)
+
+valChoice = tk.IntVar()
+valChoice.set(1)
+
+choices=[("Поиск по одному столбцу",1), ("Поиск по всему файлу",2)]
+def ShowChoice():
+    print(valChoice.get())
+
+for choice, val in choices:
+    tk.Radiobutton(myWin,
+                   text=choice,
+                   variable=valChoice,
+                   command=ShowChoice,
+                   value=val, bg=foregroundColor).place(x=200,y=520+val*20)
+
+
+#Label(frame, text='Color-Demo').pack()
 
 def clearTextInput():
     output_text.delete("1.0","end")
